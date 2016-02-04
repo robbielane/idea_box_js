faa3a7c157e223b90d85d6ecdcac36a9bae6009c
Robbie Lane <lane.robbie@gmail.com>
2016-02-04 10:27:43 -0700

    change default_scope

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,6 @@
 class Idea < ActiveRecord::Base
   has_many :idea_tags, dependent: :destroy
   has_many :tags, through: :idea_tags
-  default_scope { order('rating DESC')}
+  default_scope { order('created_at DESC')}
   enum rating: %w(swill plausable genius)
 end
